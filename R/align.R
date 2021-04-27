587c56d73b36b641af769ce5c1dd3579b9d67593
Stephen Kaluzny <spkaluzny@gmail.com>
2021-04-27 22:20:02 +0000

    version 2.5.2

--- R/align.R
+++ R/align.R
@@ -2,7 +2,9 @@
     .time_align(as(origpos, "timeDate"), as(newpos, "timeDate"),
                 as(c(how, error.how), "character"), as(matchtol, "numeric")+0)
 }
+.timealign <- timealign
 "numalign" <- function(origpos, newpos, how, error.how, matchtol = 0) {
     .num_align(as(origpos, "numeric")+0, as(newpos, "numeric")+0,
                as(c(how, error.how), "character"), as(matchtol, "numeric")+0)
 }
+.numalign <- numalign
